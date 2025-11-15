28258659176fe9d7a351c806ef31a0a9847f4a72
Nevysha <me@nevysha.art>
2025-11-15 22:47:48 +0100

    :)

--- src/database/sequelize-auto-test.ts
+++ src/database/sequelize-auto-test.ts
@@ -1,4 +1,13 @@
 import { sequelize } from '#nlpr/database/database.ts';
 import { initModels } from '#nlpr/database/models/init-models.ts';
 
-initModels(sequelize);
+const { BotMessages } = initModels(sequelize);
+
+(async () => {
+  await sequelize.sync();
+  const botMessages = await BotMessages.findAll();
+  console.log(botMessages);
+
+  const botMessage = botMessages[0];
+  console.log(botMessage.getChannel());
+})();
