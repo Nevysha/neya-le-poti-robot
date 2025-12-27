c640649e8f03a55f4940c49520f2699d75e5c753
Nevysha <me@nevysha.art>
2025-12-27 10:03:05 +0100

    :)

--- bin/run.sh
+++ bin/run.sh
@@ -2,4 +2,4 @@
 
 git pull
 pnpm run build
-NODE_ENV=prod node dist/src/autostart.js > potirobot.log 2>&1 &
+NODE_ENV=prod node dist/src/autostart.js > "potirobot.$(date +"%Y-%m-%d-%H-%M-%S").log" 2>&1 &
